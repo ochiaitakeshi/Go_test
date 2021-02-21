5ca2a0381022546d6fbae6959f8a2b0b392b8620
ochiaitakeshi <ochi@aol.jp>
2021-02-22 00:44:59 +0900

    時間変更

--- ticker.go
+++ ticker.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	t1 := time.NewTicker(1 * time.Second) // 1秒おきに通知
-	t2 := time.NewTicker(2 * time.Second) // 2秒おきに通知
+	t2 := time.NewTicker(3 * time.Second) // 3秒おきに通知
 
 	for {
 		select {
@@ -15,8 +15,8 @@ func main() {
 			// 1秒経過した。ここで何かを行う。
 			fmt.Printf("hello, world 1s\n")
 		case <-t2.C:
-			// 1秒経過した。ここで何かを行う。
-			fmt.Printf("hello, world 2s\n")
+			// 3秒経過した。ここで何かを行う。
+			fmt.Printf("hello, world 3s\n")
 		}
 	}
 	t1.Stop() // タイマを止める。
